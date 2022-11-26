c105aa6c36215e4eface9e82918e96f215ce5445
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2022-11-26 19:32:32 +0100

    flightsim.send_event supports values :)

--- flightsim/events.py
+++ flightsim/events.py
@@ -10,8 +10,8 @@ class SimEventException(Exception):
     pass
 
 
-def send_event(name: str):
+def send_event(name: str, value=0):
     """Send event to flight sim"""
 
-    log.info(f'SimEvent => {name}')
-    SimConnect.Event(name.encode('utf-8'), connect())()
+    log.info(f'SimEvent: {name} => {value}')
+    SimConnect.Event(name.encode('utf-8'), connect())(value)
