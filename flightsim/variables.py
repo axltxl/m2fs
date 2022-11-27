4fbfff43332bef9708ea7e16acde1be1c5105a93
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2022-11-27 19:33:44 +0100

    SimConnect: thread mutex and poll tick rate set to 33 Hz

--- flightsim/variables.py
+++ flightsim/variables.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import threading
+
 import SimConnect
 from .log import log
 from .sim import connect
@@ -13,22 +15,31 @@ class SimVarException(Exception):
 
 # For how long is Python-SimConnect caching
 # local SimVar data?
-SIMCONNECT_CACHE_TICK_RATE_HZ = 66
-SIMCONNECT_CACHE_TTL_MS = 1.0 / SIMCONNECT_CACHE_TICK_RATE_HZ
+SIMCONNECT_CACHE_TICK_RATE_HZ = 33
+SIMCONNECT_CACHE_TTL_MS = 1000.0 / SIMCONNECT_CACHE_TICK_RATE_HZ
 
 # SimConnect.AircraftRequests object goes in here
 # Once created, it'll update its SimVars' data from
 # SimConnect every SIMCONNECT_CACHE_TTL_MS
 __simvar_aq = None
 
+# Mutex lock is needed to have access to the
+# AircraftRequests instance (used to read/write variables)
+# since the __poll thread is also accessing this data constantly
+# (SIMCONNECT_CACHE_TICK_RATE_HZ times per second to be exact)
+__simvar_aq_mutex = threading.Lock()
+
 
 def __get_aq() -> SimConnect.AircraftRequests:
-    global __simvar_aq
-    if __simvar_aq is None:
-        __simvar_aq = SimConnect.AircraftRequests(
-            connect(), _time=SIMCONNECT_CACHE_TTL_MS
-        )
-    return __simvar_aq
+    # NOTE: critical section shared with __poll thread
+    # SEE: poll.py
+    with __simvar_aq_mutex:
+        global __simvar_aq
+        if __simvar_aq is None:
+            __simvar_aq = SimConnect.AircraftRequests(
+                connect(), _time=SIMCONNECT_CACHE_TTL_MS
+            )
+        return __simvar_aq
 
 
 class SimVar:
