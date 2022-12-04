6306347f38158be67f3a963d9a6a7af98aea563e
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2022-12-04 12:30:43 +0100

    FIX: default config file was broken (#14)

--- m2fs/cli.py
+++ m2fs/cli.py
@@ -83,7 +83,7 @@ def __parse_args(argv: list[str]) -> dict:
     Options:
         -h --help                          Show this screen.
         --version                          Show version.
-        -c, --config FILE                  Configuration file location [default: <current_working_directory>/config.py]
+        -c, --config FILE                  Configuration file location [default: {default_config_file}]
         -s, --simconnect-backend BACKEND   SimConnect client backend [default: {default_smc_backend}]
         -l, --log-level LVL                Log level [default: {default_log_level}]
     """
@@ -91,6 +91,7 @@ def __parse_args(argv: list[str]) -> dict:
     # __doc__ needs to be formatted first
     __parse_args.__doc__ = __parse_args.__doc__.format(
         pkg_name=PKG_NAME,
+        default_config_file=CLI_DEFAULT_CONFIG_FILE,
         default_smc_backend=CLI_DEFAULT_SIMCONNECT_BACKEND,
         default_log_level=CLI_DEFAULT_LOG_LEVEL,
     )
